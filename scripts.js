5990190404713191017584c41f9c1725714bd7aa
pritam1373 <ovnandy99@gmail.com>
2025-10-19 09:57:04 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 a=document.getElementById("pw");
 b=document.getElementById("np");
+
 a.addEventListener("input",function() {
     c=a.value
      if(c=="5251"){
@@ -8,5 +9,4 @@ a.addEventListener("input",function() {
      }else{
 
      }
-
 })
