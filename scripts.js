962b523b205001cdf272895f313680b231ceafdf
pritam1373 <ovnandy99@gmail.com>
2025-10-19 09:50:35 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+a=document.getElementById("pw");
+b=document.getElementById("np");
+a.addEventListener("input",function() {
+    c=a.innerHTML
+     if(c=="5251"){
+        b.style.zIndex="1"
+          
+     }else{
+
+     }
+})
